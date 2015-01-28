2e40ff43b82981e9772d70ad3a26f44bf48394c6
Derme <derme@outlook.com>
2015-01-28 12:25:23 +1100

    Initial Commit

---
+++ trunk/Assets/Scripts/ControllerArm.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ControllerArm : MonoBehaviour {
+
+    public Transform boneHumerus;
+    public Transform boneRadius;
+
+    public Slider boneAngle;
+
+    float angleCurrent;
+
+	// Use this for initialization
+	void Start () {
+        boneAngle.onValueChanged.AddListener(delegate { UpdateAngle(boneAngle.value); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        boneRadius.localRotation = Quaternion.Euler(angleCurrent, 90, 0);
+	}
+
+    public void UpdateAngle(float value) {
+        angleCurrent = value;
+    }
+}
