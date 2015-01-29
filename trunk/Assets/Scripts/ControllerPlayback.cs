1c528422925877f2c7aa1644fc2f37c39a6279e4
Derme <derme@outlook.com>
2015-01-29 15:19:51 +1100

    Added CSV Data Files

---
+++ trunk/Assets/Scripts/ControllerPlayback.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CsvReader))]
+public class ControllerPlayback : MonoBehaviour {
+
+    CsvReader csvReader;
+
+	// Use this for initialization
+    void Start() {
+        csvReader = gameObject.GetComponent<CsvReader>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
